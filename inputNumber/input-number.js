332bb2b06a8db2fdefe564c88a0c06cc8f95f4ed
lincmin <lincm69696@qq.com>
2018-08-16 15:18:24 +0800

    add inputNumber

---
+++ inputNumber/input-number.js
@@ -0,0 +1,74 @@
+
+
+function isValueNumber(value) {
+  return (/(^-?[0-9]+\.{1}\d+$)|(^-?[1-9][0-9]*$)|(^-?0{1}$)/).test(value + '')
+}
+Vue.component('input-number', {
+  template: '#input-number',
+  props: {
+    max: {
+      type: Number,
+      default: Infinity
+    },
+    min: {
+      type: Number,
+      default: -Infinity
+    },
+    step: {
+      type: Number,
+      default: 1
+    },
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: function () {
+    return {
+      currentValue: this.value
+    }
+  },
+  watch: {
+    currentValue: function (val) {
+      this.$emit('input', val)
+      this.$emit('on-change', val)
+    },
+    value: function (val) {
+      this.updateValue(val)
+    }
+  },
+  methods: {
+    updateValue: function (val) {
+      if (val > this.max) val = this.max
+      if (val < this.min) val = this.min
+      this.currentValue = val
+    },
+    handleDown: function () {
+      if (this.currentValue <= this.min) return
+      this.currentValue -= this.step
+    },
+    handleUp: function () {
+      if (this.currentValue >= this.max) return
+      this.currentValue += this.step
+    },
+    handleChange: function (event) {
+      var val = event.target.value.trim()
+      var max = this.max
+      var min = this.min
+      if (isValueNumber(val)) {
+        val = Number(val)
+        this.currentValue = val
+        if (val > max) {
+          this.currentValue = max
+        } else if (val < min) {
+          this.currentValue = min
+        }
+      } else {
+        event.target.value = this.currentValue
+      }
+    }
+  },
+  mounted: function () {
+    this.updateValue(this.value)
+  }
+})
